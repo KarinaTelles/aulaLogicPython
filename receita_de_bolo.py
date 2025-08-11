544652635e52ebf5afb3641da26b9e634425419b
Karina Telles <karinac.telles@gmail.com>
2025-08-11 16:04:58 -0300

    exemplo de implementação da receita de bolo

---
+++ receita_de_bolo.py
@@ -0,0 +1,22 @@
+#Receita de bolo em Python
+#Etapas organizadas em algoritmo
+
+# 1 - Listas os ingredientes
+ingredientes = ["2 xícaras de farinha", "1 xícara de açúcar", "1/2 xícara de manteiga", "3 ovos", "1 xícara de leite", "1 colher de sopa de fermento em pó"]
+
+print("== Ingredientes ==")
+print("Para fazer o bolo, você vai precisar dos seguintes ingredientes:")
+for ingrediente in ingredientes:
+    print("*", ingrediente)
+print("\n")
+print("Misturando os ingredientes...")
+print("1 - Bata tudo até ficar homogêneo.")
+print("2 - Adicione o fermento e misture delicadamente.")
+print("3 - Despeje a massa em uma forma untada.")
+print("4 - Leve ao forno pré-aquecido a 180°C por 30 minutos.")
+print("5 - Retire do forno e deixe esfriar.")
+
+print("\n")
+print("Assando o bolo...")
+print("\n")
+print("Seu bolo está pronto! Aproveite!")
