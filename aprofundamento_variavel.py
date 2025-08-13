b6946632ccb534bd84c04ac27b2f50ecbd6e415a
Karina Telles <karinac.telles@gmail.com>
2025-08-13 14:21:33 -0300

    aprofundamento em sobre variável

---
+++ aprofundamento_variavel.py
@@ -0,0 +1,10 @@
+#No Python, os nomes das variáveis podem conter letras, números e underscores.
+#A tipagem é dinâmica, ou seja, não é necessário declarar o tipo da variável.
+#O tipo da variável é inferido automaticamente pelo interpretador.
+int5 = 5
+string_joao = "João"
+print(int5)
+print(string_joao)
+#Ao atribuir um int e depois uma string, passa a ser uma string
+int5 = "Agora sou uma string"
+print(int5)
