45fea10c01c3f26b6e6528f2fdc0e6356e8213be
Karina Telles <karinac.telles@gmail.com>
2025-08-15 16:54:11 -0300

    adiciona o exercício 8

---
+++ ex08_maior_de_dois.py
@@ -0,0 +1,7 @@
+a = float(input("Digite o primeiro número: "))
+b = float(input("Digite o segundo número: "))
+
+if a > b:
+    print("Primeiro número é maior: ", a)
+elif b > a:
+    print("Segundo número é maior: ", b)
