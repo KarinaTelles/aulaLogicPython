912d22b7101ee3b414990623371342bb6dc66099
Karina Telles <karinac.telles@gmail.com>
2025-08-15 16:16:55 -0300

    inclui exercício 4 funcionando

---
+++ ex05_area_retangulo.py
@@ -0,0 +1,5 @@
+print("Cálculo da área do retângulo")
+base = float(input("Insira a base em cm: "))
+altura = float(input("Insira a altura em cm: "))
+area = base * altura
+print("Área do retângulo:", area)
